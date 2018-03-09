3d6980ce56d440b19af834bad87df33bd80f1d54
Frank <f.broeks@student.fontys.nl>
2018-03-09 09:00:21 +0100

    Added MVC

---
+++ GTO4Project/Assets/ResourceController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.UIElements;
+
+public class ResourceController : MonoBehaviour {
+
+    public Button Button;
+    public ResourceModel Resource;
+
+    public void RemoveResource(ResourceModel resourceModel)
+    {
+        //Resource.CurrentOwned--;
+    }
+
+    public void AddResource(ResourceModel resourceModel)
+    {
+        //Resource.CurrentOwned++;
+        resourceModel.CurrentOwned++;
+    }
+
+}
---
+++ GTO4Project/Assets/ResourceModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceModel : MonoBehaviour {
+
+    public event EventHandler handler;
+
+    public int StartAmount;
+
+    private int currentOwned;
+
+    public int CurrentOwned
+    {
+        get { return currentOwned; }
+        set {
+            currentOwned = value;
+
+            if(handler != null)
+            {
+                handler(this, new EventArgs());
+            }
+        }
+    }
+
+    
+
+	// Use this for initialization
+	void Start () {
+        currentOwned = StartAmount;
+
+
+	}
+
+    
+}
